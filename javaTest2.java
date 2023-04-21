b72fa4f2c76680b5ae5546404e3918dc933667bd
Yato <119737958+RyryaAstrey@users.noreply.github.com>
2023-04-21 17:36:11 +0900

    演習問題　構文編終了

--- javaTest2.java
+++ javaTest2.java
@@ -1,22 +1,11 @@
+import java.util.List;
+import java.util.Arrays;
+
 public class javaTest2 {
     public static void main(String[] args) throws Exception{
-        try{
-            String name = null;
-            name = name.substring(0);//[1]
-        } catch(Exception ex){
-            ex.printStackTrace();
-        }
-        try{
-            String values[] = new String[2];
-            values[2] = "value";//[2]
-        } catch(Exception ex){
-            ex.printStackTrace();
-        }
-        try{
-            Object value = "value";
-            Integer number = (Integer)value;//[3]
-        } catch(Exception ex){
-            ex.printStackTrace();
+        List<String> params = Arrays.asList(args);
+        for(String param : params){
+            System.out.println(param);
         }
     }
 }
